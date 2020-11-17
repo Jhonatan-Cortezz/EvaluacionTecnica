0a0b586cd765642fec011380410b652a446611b0
Jhonatan-Cortezz <jhonatancortez708@gmail.com>
2020-11-16 23:26:40 -0600

    Depuracion del codigo y mejora del menu

--- js/menu.js
+++ js/menu.js
@@ -16,32 +16,38 @@ if(pageTitle == "Drinks Figma"){
 console.log(pageTitle);
 
 var printMenu = `
-<nav class="navbar navbar-expand-lg navbar-dark header-nabvar">
-    <a class="navbar-brand" href="#"><i class="fas fa-grip-vertical"></i></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-    </button>
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark header-nabvar">
+        <a class="navbar-brand" href="#"><span><i class="fas fa-grip-vertical"></i></span></a>
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+    
+        <div class="row col-md-12">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto col-md-12 justify-content-center">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="${urlHome}">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Smoothies</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="${urlPupDrink}">Popular Drink</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>                              
+                </ul>              
+            </div>  
+        </div>
+    </nav>
+</div>
 
-    <div class="collapse navbar-collapse item-center" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="${urlHome}">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Smoothies</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="${urlPupDrink}">Popular Drink</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="#">About</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Contact</a>
-            </li>            
-        </ul>
-    </div>
-</nav>
 `;
 
 document.write(printMenu);
